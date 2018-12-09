7551a5f35f9f384b0302b830e65a19a72db26067
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-09 18:33:17 -0500

    Conditional Statements Algorithm - solved using if statements

--- ConditionalStatements/ConditionalStatements/Program.cs
+++ ConditionalStatements/ConditionalStatements/Program.cs
@@ -20,6 +20,25 @@ namespace ConditionalStatements
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter an integer: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if(n % 2 != 0)
+            {
+                Console.WriteLine("Weird");
+            }
+            if(n % 2 == 0 && n >= 2 && n <=5)
+            {
+                Console.WriteLine("Not Weird");
+            }
+            if(n % 2 == 0 && n >= 6 && n <= 20)
+            {
+                Console.WriteLine("Weird");
+            }
+            if(n % 2 == 0 && n > 20)
+            {
+                Console.WriteLine("Not Weird");
+            }
         }
     }
 }
