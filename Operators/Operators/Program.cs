b2a4fd7f1641e0cfc0505f54e6986bb2a699ce67
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-09 17:38:29 -0500

    Arithmetic Operators - Initial commit

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    // Arithmetic Operators 
+    // Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), 
+    // and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+
+        }
+    }
+}
