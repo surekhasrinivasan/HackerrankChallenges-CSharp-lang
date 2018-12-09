ee876d11c4a7faef10bdfffbd2dba6265b2e342d
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-09 18:18:01 -0500

    Conditional Statements Algorithm - Initial commit

---
+++ ConditionalStatements/ConditionalStatements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalStatements
+{
+    // Conditional Statements
+
+    /* Given an integer,n, perform the following conditional actions:
+
+    If n is odd, print Weird
+    If n is even and in the inclusive range of 2 to 5, print Not Weird
+    If n is even and in the inclusive range of 6 to 20, print Weird
+    If n is even and greater than 20, print Not Weird
+    Print whether or not n is weird.
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
