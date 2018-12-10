d600da795335ece64b2453e0f18ac15b1747e036
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-10 18:34:07 -0500

    Class Vs Instance Algorithm - Initial commit

---
+++ ClassVsInstance/ClassVsInstance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassVsInstance
+{
+    class Program
+    {
+        // Classes Vs. Instance
+
+        /*
+         * Write a Person class with an instance variable, age, and a constructor that takes an integer, initialAge, as a parameter. The constructor must assign initialAge to age after confirming the argument passed as initialAge is not negative; if a negative argument is passed as initialAge, the constructor should set age to 0 and print Age is not valid, setting age to 0.. In addition, you must write the following instance methods:
+
+        1. yearPasses() should increase the age instance variable by 1.
+        2. amIOld() should perform the following conditional actions:
+            If age < 13, print You are young..
+            If age >=13 and age < 18, print You are a teenager..
+            Otherwise, print You are old..
+        */
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
