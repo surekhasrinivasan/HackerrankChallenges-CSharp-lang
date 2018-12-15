6ec59ab2f45d7a691f7d55a131c14626d9879e6c
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-15 17:03:24 -0500

    Classvs.Instance Algorithm - solved

--- ClassVsInstance/ClassVsInstance/Program.cs
+++ ClassVsInstance/ClassVsInstance/Program.cs
@@ -11,7 +11,10 @@ namespace ClassVsInstance
         // Classes Vs. Instance
 
         /*
-         * Write a Person class with an instance variable, age, and a constructor that takes an integer, initialAge, as a parameter. The constructor must assign initialAge to age after confirming the argument passed as initialAge is not negative; if a negative argument is passed as initialAge, the constructor should set age to 0 and print Age is not valid, setting age to 0.. In addition, you must write the following instance methods:
+         * Write a Person class with an instance variable, age, and a constructor that takes an integer, initialAge, as a parameter. 
+         * The constructor must assign initialAge to age after confirming the argument passed as initialAge is not negative; 
+         * if a negative argument is passed as initialAge, the constructor should set age to 0 and print Age is not valid, setting age to 0.. 
+         * In addition, you must write the following instance methods:
 
         1. yearPasses() should increase the age instance variable by 1.
         2. amIOld() should perform the following conditional actions:
@@ -22,20 +25,16 @@ namespace ClassVsInstance
         
         public static void Main(string[] args)
         {
-            int T = int.Parse(Console.In.ReadLine());
-
-            for (int i = 0; i < T; i++)
+            Console.WriteLine("Enter age: ");
+            int age = int.Parse(Console.ReadLine());
+            Person p = new Person(age);
+            p.amIOld();
+            for (int j = 0; j < 3; j++)
             {
-                int age = int.Parse(Console.In.ReadLine());
-                Person p = new Person(age);
-                p.amIOld();
-                for (int j = 0; j < 3; j++)
-                {
-                    p.yearPasses();
-                }
-                p.amIOld();
-                Console.WriteLine();
+                p.yearPasses();
             }
+            p.amIOld();
+            Console.WriteLine();
         }
 
 
@@ -46,22 +45,45 @@ namespace ClassVsInstance
             public Person(int initialAge)
             {
                 // Add some more code to run some checks on initialAge
-
-                this.initialAge = age;
+                age = initialAge;               
             }
+
+            public int getAge()
+            {
+                return this.age;
+            }
+
+            public void setAge(int age)
+            {
+                this.age = age;
+            }
+
             public void amIOld()
             {
                 // Do some computations in here and print out the correct statement to the console 
-                if (age < 0)
+                if (this.age < 0)
                 {
-                    Console.WriteLine("Age is not valid, setting age to 0");
+                    age = 0;
+                    Console.WriteLine("Age is not valid, setting age to 0.");
                 }
-                else if (age)
+                else if (this.age < 13)
+                {
+                    Console.WriteLine("You are young.");
+                }
+                else if(this.age >= 13 && this.age < 18)
+                {
+                    Console.WriteLine("You are a teenager.");
+                }
+                else
+                {
+                    Console.WriteLine("You are old.");
+                }
             }
 
             public void yearPasses()
             {
                 // Increment the age of the person in here
+                setAge(getAge() + 1);
             }
         }
     }
