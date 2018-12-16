4d6a77295495e23fd3b874622d89a5f657562d03
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-16 17:42:06 -0500

    Loops algorithm - initial commit'

---
+++ loops/loops/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops
+{
+    // Loops
+
+    // Given an integer, n, print its first 10 multiples. Each multiple n x i(where 1 <=i<= 10) should be 
+    //printed on a new line in the form: n x i = result.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
