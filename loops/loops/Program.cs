733e98a789e9d2c66a75fde105fe8e00fe24a5bf
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-16 20:24:20 -0500

    Loops algorithm - completed'

--- loops/loops/Program.cs
+++ loops/loops/Program.cs
@@ -9,13 +9,20 @@ namespace loops
     // Loops
 
     // Given an integer, n, print its first 10 multiples. Each multiple n x i(where 1 <=i<= 10) should be 
-    //printed on a new line in the form: n x i = result.
+    // printed on a new line in the form: n x i = result.
 
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter an integer: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
 
+            for (int i = 1; i <=10; i++)
+            {
+                Console.WriteLine("{0} {1} {2} = {3}", n, 'x', i, n * i);
+            }
         }
     }
 }
