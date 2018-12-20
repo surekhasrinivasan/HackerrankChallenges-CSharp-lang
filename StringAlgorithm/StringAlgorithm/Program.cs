8d12dbc5f43b75b8f450367144956e4e985eeb1b
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-20 17:35:08 -0500

    String algorithm-initial commit

---
+++ StringAlgorithm/StringAlgorithm/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAlgorithm
+{
+    class Program
+    {
+        /* Given a string, S, of length N that is indexed from 0 to N-1, print its even-indexed 
+         * and odd-indexed characters as 2 space-separated strings on a single line.
+        *  Note: 0 is considered to be an even index.
+        */
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
