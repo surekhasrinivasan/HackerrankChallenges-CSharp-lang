1efc9fcf7075093f22d6e90a1932f7d8144f5afa
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-20 15:53:05 -0500

    Program to break a string to characters

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    // Example program to break a String down into its component characters, we can use the String.toCharArray method
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mystr = "I see an apple on the tree.";
+            char[] myCharArray = mystr.ToCharArray();
+            
+            for(int i = 0; i < mystr.Length; i++)
+            {
+                // Print each sequential character on the same line. 
+                Console.Write(myCharArray[i]);                
+            }
+            
+            // Prints a new line
+            Console.WriteLine();
+        }
+    }
+}
