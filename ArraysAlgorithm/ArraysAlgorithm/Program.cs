7bb25cf0c0b753b34424557e13a1dfe9a262b98e
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-21 17:07:18 -0500

    Reverse elements in an array-solved using Reverse method

--- ArraysAlgorithm/ArraysAlgorithm/Program.cs
+++ ArraysAlgorithm/ArraysAlgorithm/Program.cs
@@ -11,6 +11,21 @@ namespace ArraysAlgorithm
         // Given an array, A, of N integers, print A's elements in reverse order as a single line of space-separated numbers.
         static void Main(string[] args)
         {
+            int[] arrNum = { 8, 3, 6, 7 };
+
+            foreach(int value in arrNum)
+            {
+                Console.Write(value);
+            }
+            Console.WriteLine();
+
+            Array.Reverse(arrNum);
+
+            foreach(int value in arrNum)
+            {
+                Console.Write(value);
+            }
+            Console.WriteLine();
         }
     }
 }
