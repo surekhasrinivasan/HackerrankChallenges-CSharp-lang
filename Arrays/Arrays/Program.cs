8f0c52f50bc743d6e85af141353faf910e57bfaa
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-21 15:32:41 -0500

    Example program on Arrays

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        // Example for Arrays
+        static void Main(string[] args)
+        {
+            // the numbers of elements we want to hold 
+            int arraySize = 4;
+
+            // our array declaration
+            string[] stringArray = new string[arraySize]; 
+
+            for(int i = 0; i < arraySize; i++)
+            {
+                // assign value to Index i
+                stringArray[i] = "This is stored in index " + i;
+
+                // Print value saved in index i
+                Console.WriteLine(stringArray[i]);
+            }
+        }
+    }
+}
