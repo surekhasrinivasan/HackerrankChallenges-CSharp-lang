81019d31aa80d371ac80545c249f13d9db332ba0
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-22 07:14:20 -0500

    Reverse elements in an array-solved without using Reverse method

--- ArraysAlgorithm/ArraysAlgorithm/Program.cs
+++ ArraysAlgorithm/ArraysAlgorithm/Program.cs
@@ -11,21 +11,54 @@ namespace ArraysAlgorithm
         // Given an array, A, of N integers, print A's elements in reverse order as a single line of space-separated numbers.
         static void Main(string[] args)
         {
-            int[] arrNum = { 8, 3, 6, 7 };
+            // using reverse method.
+            //int[] arrNum = { 8, 3, 6, 7 };
 
-            foreach(int value in arrNum)
+            //foreach(int value in arrNum)
+            //{
+            //    Console.Write(value);
+            //}
+            //Console.WriteLine();
+
+            //Array.Reverse(arrNum);
+
+            //foreach(int value in arrNum)
+            //{
+            //    Console.Write(value);
+            //}
+            //Console.WriteLine();
+
+            // Without using reverse method
+            int[] arr = new int[55];
+
+            int i, j, n, temp;
+
+            Console.WriteLine("Enter the number of elements in an array: ");
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the elements of the array: ");
+            for(i = 0; i < n; i++)
             {
-                Console.Write(value);
+                arr[i] = int.Parse(Console.ReadLine());
             }
-            Console.WriteLine();
 
-            Array.Reverse(arrNum);
+            j = i - 1;
 
-            foreach(int value in arrNum)
+            for(i = 0; i < j; i++)
             {
-                Console.Write(value);
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j--;
             }
-            Console.WriteLine();
+
+            Console.WriteLine("Elements after reversing the array: ");
+            for(i = 0; i < n; i++)
+            {
+                Console.Write(" " + arr[i]);
+                
+            }
+            Console.ReadLine();
         }
     }
 }
