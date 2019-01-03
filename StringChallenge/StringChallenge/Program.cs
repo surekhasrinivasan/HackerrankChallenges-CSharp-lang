8275f832301ed1b2cd89114ad533005e97c4072c
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-03 17:17:34 -0500

    Find FirstNonRepeatCharacter algorithm - initial commit

---
+++ StringChallenge/StringChallenge/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StringChallenge
+{
+    // Given a string find the first non-repeating character and return the character, if not return underscore
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
