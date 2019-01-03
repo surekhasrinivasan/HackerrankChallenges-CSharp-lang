28330735d6a41e74c5aef28a21449086386c0153
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-03 17:41:04 -0500

    function firstNonRepeatCharacter is implemented

--- StringChallenge/StringChallenge/Program.cs
+++ StringChallenge/StringChallenge/Program.cs
@@ -7,7 +7,40 @@ namespace StringChallenge
     {
         static void Main()
         {
+            char firstNonRepeatChar = firstNonRepeatingCharacter("abcdabc");
+            Console.WriteLine(firstNonRepeatChar);
+        }
 
+        // function loops through the string and returns first non-repeating character 
+        public static char firstNonRepeatingCharacter(string str)
+        {
+            char letter;
+            bool seen = false;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                letter = str[i];
+                for (int j = 0; j < str.Length; j++)
+                {
+                    if (str[j] == letter)
+                    {
+                        if (seen)
+                        {
+                            seen = false;
+                            break;
+                        }
+                        seen = true;
+                    }
+                }
+
+                if (seen)
+                {
+                    //Console.WriteLine(letter); // return
+                    return letter;
+                }
+            }
+            //Console.WriteLine('_');
+            return '_';
         }
     }
 }
