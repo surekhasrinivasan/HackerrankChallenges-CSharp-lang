f8a407a0206e5c7fb03d31e88dcf0cf4b840fc06
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-05 18:54:04 -0500

    Reverse the elements of an array solved

--- ArraysAlgorithm/ArraysAlgorithm/Program.cs
+++ ArraysAlgorithm/ArraysAlgorithm/Program.cs
@@ -29,22 +29,54 @@ namespace ArraysAlgorithm
             //Console.WriteLine();
 
             // Without using reverse method
-            int[] arr = new int[55];
+            //    int[] arr = new int[20];
 
-            int i, j, n, temp;
+            //    int i, j, n, temp;
 
-            Console.WriteLine("Enter the number of elements in an array: ");
-            n = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("Enter the number of elements in an array: ");
+            //    n = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the elements of the array: ");
+            //    Console.WriteLine("Enter the elements of the array: ");
+            //    for(i = 0; i < n; i++)
+            //    {
+            //        arr[i] = int.Parse(Console.ReadLine());
+            //    }
+
+            //    j = i - 1;
+
+            //    for(i = 0; i < j; i++)
+            //    {
+            //        temp = arr[i];
+            //        arr[i] = arr[j];
+            //        arr[j] = temp;
+            //        j--;
+            //    }
+
+            //    Console.WriteLine("Elements after reversing the array: ");
+            //    for(i = 0; i < n; i++)
+            //    {
+            //        Console.Write(" " + arr[i]);                
+            //    }
+            //    Console.ReadLine();
+            //}
+
+            Console.WriteLine("Enter number of elements in an array : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter array elements: ");
+            //int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            string[] arrTemp = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arrTemp, Int32.Parse);
+            int i, j, temp;
+
             for(i = 0; i < n; i++)
             {
-                arr[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine(arr[i]);
             }
 
             j = i - 1;
 
-            for(i = 0; i < j; i++)
+            for (i = 0; i < j; i++)
             {
                 temp = arr[i];
                 arr[i] = arr[j];
@@ -53,10 +85,10 @@ namespace ArraysAlgorithm
             }
 
             Console.WriteLine("Elements after reversing the array: ");
-            for(i = 0; i < n; i++)
+            for (i = 0; i < n; i++)
             {
-                Console.Write(" " + arr[i]);
-                
+                Console.Write(arr[i]);
+                Console.Write(' ');
             }
             Console.ReadLine();
         }
