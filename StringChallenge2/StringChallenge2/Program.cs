1c5a120c033af6a9bef021f48ba7e04da7e1675b
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-05 17:59:33 -0500

    StringChallenge - initial commit

---
+++ StringChallenge2/StringChallenge2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringChallenge2
+{
+    // Coding Challenge - Compare Strings
+
+    /**
+     * Create the following function/method:
+
+     * functionName(string, string, length)
+     * function should return an array of integers the length of the string
+     * function is comparing how to change the first word into the second word by returning the number of steps each character must take to become the character of the other string.
+     * Example:
+        function("this", "that", 4) = [0, 0, -8, 1]
+        function('this', bear', 4) = [-18, -3, -8, -1]
+        function('"bear", 'this", 4) = [18, 3, 8, 1]
+     * Also return the total change score. This should add together the changes in the array to give a total change score
+       function("bear", "this", 4) = 25
+     * */
+     
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }       
+    }
+}
