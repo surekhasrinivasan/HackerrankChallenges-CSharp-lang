2c2d070721700d444cead7c7eafd759d72938ff0
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-06 22:43:04 -0500

    Prints the duplicate integers in the array

--- FindDuplicatesChallenge/FindDuplicatesChallenge/Program.cs
+++ FindDuplicatesChallenge/FindDuplicatesChallenge/Program.cs
@@ -9,8 +9,24 @@ namespace FindDuplicatesChallenge
     // Given an array of integers, Find the duplicates in the array and the number of its occurrences 
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int[] arr = { 2, 1, 3, 5, 3, 2 };
+
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                for (int j = i; j < arr.Length - 1; j++)
+                {
+                    if (arr[i] == arr[j + 1])
+                    {
+                        count++;
+                        Console.WriteLine("The duplicate integers are: " + arr[i]);
+                    }
+                }
+            }
         }
     }
 }
