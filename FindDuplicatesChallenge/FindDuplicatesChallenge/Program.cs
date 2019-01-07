872d12183e635f828fafddfb904e1f917f2d4fa2
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-06 22:39:05 -0500

    FindDuplicateChallenge algorithm - initial commit

---
+++ FindDuplicatesChallenge/FindDuplicatesChallenge/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindDuplicatesChallenge
+{
+    // Given an array of integers, Find the duplicates in the array and the number of its occurrences 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
