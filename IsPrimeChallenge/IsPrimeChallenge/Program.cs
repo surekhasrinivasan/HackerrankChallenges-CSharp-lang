b275ebea7b6912b6e9007ae52d91e9672ca31ec7
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-08 13:43:04 -0500

    IsPrime algorithm - initial commit

---
+++ IsPrimeChallenge/IsPrimeChallenge/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsPrimeChallenge
+{
+    class Program
+    {
+        // Given a number find if it is Prime number or not 
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
