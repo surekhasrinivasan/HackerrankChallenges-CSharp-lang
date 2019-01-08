e39000f7075c28e0a214a932338aec1e486b51e8
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-08 13:55:56 -0500

    Algorithm returns Prime if it is a prime number

--- IsPrimeChallenge/IsPrimeChallenge/Program.cs
+++ IsPrimeChallenge/IsPrimeChallenge/Program.cs
@@ -11,7 +11,28 @@ namespace IsPrimeChallenge
         // Given a number find if it is Prime number or not 
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
 
+            bool isprime = true;
+
+            for(int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if(number % i == 0)
+                {
+                    isprime = false;
+                    break;
+                }
+            }
+
+            if (isprime)
+            {
+                Console.WriteLine("Number is PRIME");
+            }
+            else
+            {
+                Console.WriteLine("Number is NOT PRIME");
+            }
         }
     }
 }
