be6938b8252522b63466a22d39b4155fa66c5f1e
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-08 14:30:15 -0500

    Demo of Jump statments - Break demo

---
+++ JumpStatements/JumpStatements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpStatements
+{
+    // Jump statements are used to jump to a particular position in a program. Break, Continue, goto and Return are the jump statements
+    
+    // Program to show Jump statements 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Break is used to terminate the loop and switch statement
+            for(int i = 1; i <=10; i++)
+            {
+                if (i == 5)
+                    break;
+                Console.WriteLine("i is: " + i);
+            }
+        }
+    }
+}
