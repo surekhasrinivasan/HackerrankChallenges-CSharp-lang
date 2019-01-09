521644346307c582e1a5c2fe1db9ab401f60ef19
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-09 17:49:12 -0500

    Sorting Array in Ascending order

--- SortingArrays/SortingArrays/Program.cs
+++ SortingArrays/SortingArrays/Program.cs
@@ -8,8 +8,29 @@ namespace SortingArrays
 {
     class Program
     {
+        // Given an array, sort it in ascending order or descending order
         static void Main(string[] args)
         {
+            // sorting in ascending order
+            int[] arr = new int[] { 7, 3, 5, 1, 9, 2 };
+            int temp;
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if(arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            foreach(int item in arr)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
