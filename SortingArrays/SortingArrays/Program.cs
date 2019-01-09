ba7ac906bbeeff38188f81a318dcf85eb14cbdf5
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-09 17:56:57 -0500

    Sorting Array in descending order

--- SortingArrays/SortingArrays/Program.cs
+++ SortingArrays/SortingArrays/Program.cs
@@ -27,7 +27,32 @@ namespace SortingArrays
                     }
                 }
             }
+
+            Console.WriteLine("Ascending order sorting");
             foreach(int item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+            // sorting in descending order
+            //int[] arr1 = new int[] { 7, 3, 5, 1, 9, 2 };
+            //int temp1;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] < arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Descending order sorting");
+            foreach (int item in arr)
             {
                 Console.WriteLine(item);
             }
