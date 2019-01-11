55b32628a0568f3a06b7f476f695bf7072bf65c7
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-11 16:56:30 -0500

    Demo - Two dimensional array

--- TypesOfArrays/TypesOfArrays/Program.cs
+++ TypesOfArrays/TypesOfArrays/Program.cs
@@ -12,6 +12,16 @@ namespace TypesOfArrays
         static void Main(string[] args)
         {
             // Multidimension Array - Two dimension Array
+            int[,] arr = new int[3, 4] { { 1, 2, 3, 4 }, { 10, 20, 30, 40 }, { 11, 22, 33, 44 } };
+
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
