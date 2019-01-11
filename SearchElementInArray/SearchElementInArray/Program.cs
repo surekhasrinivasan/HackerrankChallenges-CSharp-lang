735b0574aa89a963ad7268a7d93be38a3aaf3654
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-10 22:39:58 -0500

    Program to find if an element exists in the array or not

---
+++ SearchElementInArray/SearchElementInArray/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchElementInArray
+{
+    // Linear Search 
+
+    // Given an array, Search if the element is in the array or not 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 7, 3, 5, 1, 9, 2 };
+
+            Console.WriteLine("Enter the number to search: ");
+            int num = int.Parse(Console.ReadLine());
+
+            bool found = false;
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(num == arr[i])
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (found)
+            {
+                Console.WriteLine("Element found");
+            }
+            else
+            {
+                Console.WriteLine("Element not found");
+            }
+        }
+    }
+}
