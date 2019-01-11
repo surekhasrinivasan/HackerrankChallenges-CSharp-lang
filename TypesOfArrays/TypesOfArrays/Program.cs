1508a83c9d45fe38c650c31e32c6f83c6413c2e4
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-11 17:02:37 -0500

    Demo of Jagged Array

--- TypesOfArrays/TypesOfArrays/Program.cs
+++ TypesOfArrays/TypesOfArrays/Program.cs
@@ -11,17 +11,35 @@ namespace TypesOfArrays
     {
         static void Main(string[] args)
         {
-            // Multidimension Array - Two dimension Array
-            int[,] arr = new int[3, 4] { { 1, 2, 3, 4 }, { 10, 20, 30, 40 }, { 11, 22, 33, 44 } };
+            //// Multidimension Array - Two dimension Array
+            //int[,] arr = new int[3, 4] { { 1, 2, 3, 4 }, { 10, 20, 30, 40 }, { 11, 22, 33, 44 } };
 
-            for(int i = 0; i < 3; i++)
+            //for(int i = 0; i < 3; i++)
+            //{
+            //    for(int j = 0; j < 4; j++)
+            //    {
+            //        Console.Write(arr[i, j] + "\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            // Jagged array - Array of Arrays
+
+            int[][] arr = new int[3][];
+
+            arr[0] = new int[3] { 1, 2, 3 };
+            arr[1] = new int[2] { 10, 20 };
+            arr[2] = new int[4] { 11, 22, 33, 44 };
+
+            foreach(int[] ar in arr)
             {
-                for(int j = 0; j < 4; j++)
+                foreach(int item in ar)
                 {
-                    Console.Write(arr[i, j] + "\t");
+                    Console.Write(item + "\t");
                 }
                 Console.WriteLine();
             }
+
         }
     }
 }
