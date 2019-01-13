57603d359bdd56227393d40c415148c9cf233873
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-12 22:16:41 -0500

    Usage of string constructor

--- StringsMethods/StringsMethods/Program.cs
+++ StringsMethods/StringsMethods/Program.cs
@@ -22,6 +22,18 @@ namespace StringsMethods
             // Usage of string.concat()method
             string fullName = string.Concat(fname, lname);
             Console.WriteLine("Full Name: {0}", fullName);
+
+            // Usage of string constructor 
+            // convert character array to string 
+            char[] letters = { 'S', 'U', 'R', 'E', 'K', 'H', 'A' };
+            string hello = new string(letters);
+            Console.WriteLine("Hello : {0}", hello);
+
+            // convert string to character array
+            char[] ch = hello.ToCharArray();
+            Console.WriteLine(ch);
+
+
         }
     }
 }
