8b6f12beae5b0438544ad56fda9cb32bd796e246
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-14 21:43:26 -0500

    Algorithm to calculate percent

---
+++ CalculatePercent/CalculatePercent/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatePercent
+{
+    // Demo program to show implentatio of class 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
