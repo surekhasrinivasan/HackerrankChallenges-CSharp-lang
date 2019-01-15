4c4be49103f5e1f3be4c80cdec6fa4d7f1b72bbb
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-14 22:14:40 -0500

    CalculatePercent method created

--- CalculatePercent/CalculatePercent/Program.cs
+++ CalculatePercent/CalculatePercent/Program.cs
@@ -6,13 +6,20 @@ using System.Threading.Tasks;
 
 namespace CalculatePercent
 {
-    // Demo program to show implentatio of class 
+    // Demo program to show implentation of class 
     class Program
     {
+        int marks;
+        void CalculatePercent()
+        {
+            int percent = marks * 100 / 50;
+            Console.WriteLine(percent);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+
+            
         }
-    }
+    }    
 }
