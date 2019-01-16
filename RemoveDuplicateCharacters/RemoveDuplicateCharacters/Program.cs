50311e8f16c4541eb6e2ff37505e63e9e9e0f4e9
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-15 22:08:47 -0500

    RemoveDuplicateCharacters - initial commit

---
+++ RemoveDuplicateCharacters/RemoveDuplicateCharacters/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicateCharacters
+{
+    // Program removes duplicate chars using string concats.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }        
+    }
+}
