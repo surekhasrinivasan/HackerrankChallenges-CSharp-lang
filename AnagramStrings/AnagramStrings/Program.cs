c67e2b3d3ce31c5faa19d815094e0c7db176275d
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-19 22:03:12 -0500

    Anagram strings - initial commit

---
+++ AnagramStrings/AnagramStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramStrings
+{
+    // Program to show if two strings are anagram to each other.
+
+    // Two words are said to be Anagrams of each other if they share the same set of letters to form the respective words.
+    // for an example: Silent–>Listen, post–>opts.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+    }
+}
