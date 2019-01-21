5fc08dc35cacb9c76796f0c7dbee13f3193c7d0d
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-21 18:07:08 -0500

    Calculate the Power Of a Number - initial commit

---
+++ PowerOfNumber/PowerOfNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOfNumber
+{
+    // Program to calculate the power of a number using recursion
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = PowerOfANumber(6, 5);
+            Console.WriteLine(result);
+        }
+
+        
+    }
+}
