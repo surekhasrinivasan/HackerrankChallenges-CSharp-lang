b91893d3bf287a9df9e81ad0a3e61346cb8fb33a
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-21 18:08:17 -0500

    Calculate the Power Of a Number - solved

--- PowerOfNumber/PowerOfNumber/Program.cs
+++ PowerOfNumber/PowerOfNumber/Program.cs
@@ -11,10 +11,29 @@ namespace PowerOfNumber
     {
         static void Main(string[] args)
         {
-            double result = PowerOfANumber(6, 5);
+            double result = PowerOfANumber(5, 4);
             Console.WriteLine(result);
         }
 
-        
+        public static double PowerOfANumber(double baseNumber, int exponent)
+        {
+            if (exponent < 0)
+            {
+                Console.Error.WriteLine("Usage of this function is limited to positive exponents only");
+                throw new Exception();
+            }
+            else if (exponent == 0)
+            {
+                return 1;
+            }
+            else if (exponent == 1)
+            {
+                return baseNumber;
+            }
+            else
+            {
+                return baseNumber * PowerOfANumber(baseNumber, exponent - 1);
+            }
+        }
     }
 }
