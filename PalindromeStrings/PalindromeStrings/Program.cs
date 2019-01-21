1e5216863344bf710849848fd63a52d8cc7488b9
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-20 23:50:59 -0500

    Another way of solving - PalindromeStrings algorithm

--- PalindromeStrings/PalindromeStrings/Program.cs
+++ PalindromeStrings/PalindromeStrings/Program.cs
@@ -11,24 +11,54 @@ namespace PalindromeStrings
     // A palindrome has the same letters on both ends of the string. ex: kayak, malayalam
     class Program
     {
+        public static bool IsPalindrome(string word)
+        {
+            int min = 0;
+            int max = word.Length - 1;
+
+            while (true)
+            {
+                if(min > max)
+                {
+                    return true;
+                }
+                char a = word[min];
+                char b = word[max];
+                if(char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
+
         static void Main()
         {
-            Console.WriteLine("Enter a word: ");
-            string word = Console.ReadLine();
-            
-            // Convert the word to char array 
-            var array = word.ToCharArray();
+            //Console.WriteLine("Enter a word: ");
+            //string word = Console.ReadLine();
 
-            // Reverse the array
-            Array.Reverse(array);
+            //// Convert the word to char array 
+            //var array = word.ToCharArray();
 
-            if (word.Equals(new string(array)))
+            //// Reverse the array
+            //Array.Reverse(array);
+
+            //if (word.Equals(new string(array)))
+            //{
+            //    Console.WriteLine(word + " is Palindrome");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(word + " is not Palindrome");
+            //}
+
+            // Another version
+            string[] array = {"civic", "deleveled", "Hannah", "kayak", "level", "examiron", "racecar", "radar", "refer", "reviver",
+                               "easywcf", "rotator", "rotor", "sagas", "solos", "stats", "tenet", "Csharpstar", ""};
+            foreach (string word in array)
             {
-                Console.WriteLine(word + " is Palindrome");
-            }
-            else
-            {
-                Console.WriteLine(word + " is not Palindrome");
+                Console.WriteLine("{0} = {1}", word, IsPalindrome(word));
             }
         }
     }
