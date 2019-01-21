3660452a54f0c37844d339f5746918b377c54a53
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-21 17:35:04 -0500

    Algorithm to find the factors of a number - solved

---
+++ FactorsOfANumber/FactorsOfANumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorsOfANumber
+{
+    // Program to find the factors of a number 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The factors are: ");
+
+            for(int i = 1; i <= num; i++)
+            {
+                if(num % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
