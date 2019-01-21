af69493231bf99248ddc2b7f30876a5082f796c8
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-20 22:32:46 -0500

    Algorithm PalindromeStrings - initial commit

---
+++ PalindromeStrings/PalindromeStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeStrings
+{
+    // Program to find out if the string is palindromic or not
+
+    // A palindrome has the same letters on both ends of the string. ex: kayak, malayalam
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
