4b1183cfcc03f772cba9709fa463807e8e924802
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-24 20:24:38 -0500

    Fibonacci series - implemented

--- FibonacciNumbers/FibonacciNumbers/Program.cs
+++ FibonacciNumbers/FibonacciNumbers/Program.cs
@@ -12,8 +12,39 @@ namespace FibonacciNumbers
     // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34… The next number is found by adding up the two numbers before it.
     class Program
     {
+        static int FibonacciSeries(int n)
+        {
+            int firstnumber = 0,
+                secondnumber = 1,
+                result = 0;
+
+            // To return the first Fibonacci number
+            if (n == 0)
+                return 0;
+
+            // To return the second Fibonacci number
+            if (n == 1)
+                return 1;
+
+            for(int i = 2; i <= n; i++)
+            {
+                result = firstnumber + secondnumber;
+                firstnumber = secondnumber;
+                secondnumber = result;
+            }
+            return result;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the length of the Fibonacci Series: ");
+            int len = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < len; i++)
+            {
+                Console.Write("{0} \t", FibonacciSeries(i));
+            }
+            Console.WriteLine();
         }
     }
 }
