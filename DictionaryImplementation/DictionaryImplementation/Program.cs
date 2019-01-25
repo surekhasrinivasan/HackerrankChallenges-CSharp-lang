f3a48a4af69fd97b88fc67d3c71c920c2d9845f9
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-24 21:34:36 -0500

    Dictionary Implementation - initial commit

---
+++ DictionaryImplementation/DictionaryImplementation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryImplementation
+{
+    // Program to demonstrate Dictionary 
+
+    /*
+     * The Dictionary in C# is a collection of Keys and Values, where key is like word and value is like definition.
+     * 
+     * The Dictionary<TKey, TValue> class is a generic collection class in the System.Collection.Generics namespace. 
+     * TKey denotes the type of key and TValue is the type of TValue.
+     * */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
