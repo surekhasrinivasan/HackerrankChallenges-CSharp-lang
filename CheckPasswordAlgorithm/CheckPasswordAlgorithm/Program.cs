a0fa660b9982355243712d5c7f852bdf8e4b9a22
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-26 17:47:53 -0500

    CheckPassword Algorithm - initial commit

---
+++ CheckPasswordAlgorithm/CheckPasswordAlgorithm/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPasswordAlgorithm
+{
+    // Program to check password
+
+    /*
+     * The password should have:
+     * 1. min 6 char and max 12 char
+     * 2. No two similar chars consecutively
+     * 3. 1 lower case
+     * 4. 1 upper case
+     * 5. 1 special char
+     * 6. No white space
+    **/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    }
