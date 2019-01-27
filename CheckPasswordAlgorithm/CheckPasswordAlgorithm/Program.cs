e00622b2f26b7fd6410178315c6c122f13d5d871
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-26 21:41:13 -0500

    CheckPassword Algorithm - solved

--- CheckPasswordAlgorithm/CheckPasswordAlgorithm/Program.cs
+++ CheckPasswordAlgorithm/CheckPasswordAlgorithm/Program.cs
@@ -17,10 +17,47 @@ namespace CheckPasswordAlgorithm
      * 5. 1 special char
      * 6. No white space
     **/
-    class Program
+    public static class Program
     {
+        public static bool CheckPassword(string password)
+        {
+            // Min 6 char and max 12 char
+            if (password.Length < 6 || password.Length > 12)
+                return false;
+
+            // one upper case
+            if (!password.Any(char.IsUpper))
+                return false;
+
+            // one lower case
+            if (!password.Any(char.IsLower))
+                return false;
+
+            // No white space
+            if (password.Contains(" "))
+                return false;
+
+            // No two similar chars consecutively
+            for (int i = 0; i < password.Length - 1; i++)
+            {
+                if (password[i] == password[i + 1])
+                    return false;
+            }
+
+            string specialCharacters = @"%!@#$%^&*()?/>.<,:;'\|}]{[_~`+=-" + "\"";
+            char[] specialCharactersArray = specialCharacters.ToArray();
+
+            foreach (char sc in specialCharacters)
+            {
+                if (password.Contains(sc))
+                    return true;
+            }
+
+            return false;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine(CheckPassword("Alpha$"));            
         }
     }
     }
