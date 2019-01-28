031b255e406e3d5588da325370b22a8af210c70e
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-27 22:01:17 -0500

    ReturnRemainder - initial commit

---
+++ ReturnRemainder/ReturnRemainder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnRemainder
+{
+    // Program to accept 2 integers and get remainder back
+
+    // Need to handle System.DivideByZeroException
+    // Need to handle case where the dividend(x) is less than the divisor (y)
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
