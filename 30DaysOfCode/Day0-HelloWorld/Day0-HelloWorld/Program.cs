3a05c27d8d16a5021586f84387c08a79845dde24
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-16 11:25:54 -0500

    Day0-Hello World program-completed

---
+++ 30DaysOfCode/Day0-HelloWorld/Day0-HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day0_HelloWorld
+{
+    // Prgram to print Hello World 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare a string variable which takes input from user
+            string input_string;
+
+            // Take input from user 
+            Console.WriteLine("Enter a string: ");
+            input_string = Console.ReadLine();
+
+            // Print string literal "Hello World"
+            Console.WriteLine("Hello World");
+
+            // print the user input 
+            Console.WriteLine(input_string);
+        }
+    }
+}
