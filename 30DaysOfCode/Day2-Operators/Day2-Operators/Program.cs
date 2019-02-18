6d612f0c1d45616305d6c900a26b338c064628d7
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-18 18:21:08 -0500

    Day2-Operators - initial commit

---
+++ 30DaysOfCode/Day2-Operators/Day2-Operators/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_Operators
+{
+    // Arithmetic Operators
+    // Print the total meal cost, where totalCost is the rounded integer result of the entire bill (mealCost with added tax and tip)
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
