2e0ee8b271d4a0a146a993eb7fcfd3e0d7388735
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-18 18:06:27 -0500

    Day1-DataTypes - initial commit

---
+++ 30DaysOfCode/Day1-DataTypes/Day1-DataTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_DataTypes
+{
+    // Print the sum of both integers on the first line, the sum of both doubles (scaled to 1 decimal place) 
+    // on the second line, and then the two concatenated strings on the third line.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
