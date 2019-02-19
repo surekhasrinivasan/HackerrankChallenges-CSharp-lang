ff65879187f795404ccef3283b5cdf7c980b3e7e
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-19 11:58:16 -0500

    Day5-Loops - solved

---
+++ 30DaysOfCode/Day5-Loops/Day5-Loops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_Loops
+{
+    // Loops
+
+    // Given an integer, n, print its first 10 multiples. Each multiple n x i(where 1 <=i<= 10) should be 
+    // printed on a new line in the form: n x i = result.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= 10; i++)
+            {
+               Console.WriteLine("{0} x {1} = {2}", n, i, n * i);
+            }
+        }
+    }
+}
