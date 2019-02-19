0c761871eb263f3990f0b97aeb10326a43af053c
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-19 10:42:57 -0500

    Day2-Operators - solved

--- 30DaysOfCode/Day2-Operators/Day2-Operators/Program.cs
+++ 30DaysOfCode/Day2-Operators/Day2-Operators/Program.cs
@@ -12,7 +12,30 @@ namespace Day2_Operators
     {
         static void Main(string[] args)
         {
+            double meal_cost = Convert.ToDouble(Console.ReadLine());
 
+            int tip_percent = Convert.ToInt32(Console.ReadLine());
+
+            int tax_percent = Convert.ToInt32(Console.ReadLine());
+
+            solve(meal_cost, tip_percent, tax_percent);
+        }
+
+        // Complete the solve function below.
+        public static void solve(double meal_cost, int tip_percent, int tax_percent)
+        {
+
+            double tip;
+            tip = (meal_cost * tip_percent) / 100;
+
+            double tax;
+            tax = (meal_cost * tax_percent) / 100;
+
+            double total_cost;
+
+            total_cost = meal_cost + tip + tax;
+
+            Console.WriteLine(Math.Round(total_cost));
         }
     }
 }
