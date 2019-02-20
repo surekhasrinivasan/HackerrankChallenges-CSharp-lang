eac7a597ecb4501c722e2283b430905677984cf5
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-20 07:51:08 -0500

    Day8-DictionariesAndMaps - initial commit

---
+++ 30DaysOfCode/Day8-DictionariesAndMaps/Day8-DictionariesAndMaps/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8_DictionariesAndMaps
+{
+    /*
+     * Given n names and phone numbers, assemble a phone book that maps friends' names to their respective phone numbers. 
+     * You will then be given an unknown number of names to query your phone book for. For each name queried, 
+     * print the associated entry from your phone book on a new line in the form name=phoneNumber; 
+     * if an entry for name is not found, print Not found instead.
+
+     * Note: Your phone book should be a Dictionary/Map/HashMap data structure.
+     * */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
