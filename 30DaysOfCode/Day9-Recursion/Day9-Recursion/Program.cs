1ad19fc4c9bdbb381a5411622af91268e082c538
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-21 11:12:21 -0500

    Day9-Recursion - initial commit

---
+++ 30DaysOfCode/Day9-Recursion/Day9-Recursion/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9_Recursion
+{
+    // Write a factorial function that takes a positive integer, N as a parameter and prints the result of N! (N factorial).
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
