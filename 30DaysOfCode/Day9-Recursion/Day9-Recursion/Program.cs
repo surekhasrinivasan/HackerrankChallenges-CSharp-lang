a82c3826b1f5458dc48330c9555ef4309bd66c68
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-21 11:18:55 -0500

    Day9-Recursion - solved that passes Hackerrank testcases

--- 30DaysOfCode/Day9-Recursion/Day9-Recursion/Program.cs
+++ 30DaysOfCode/Day9-Recursion/Day9-Recursion/Program.cs
@@ -9,9 +9,33 @@ namespace Day9_Recursion
     // Write a factorial function that takes a positive integer, N as a parameter and prints the result of N! (N factorial).
     class Program
     {
+        // Complete the factorial function below.
+        static int factorial(int n)
+        {
+            // Base Case
+            if (n <= 1)
+            {
+                return 1;
+            }
+            // Recursive Case
+            else
+            {
+                return n * factorial(n - 1);
+            }
+        }
+
         static void Main(string[] args)
         {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = factorial(n);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
         }
     }
 }
