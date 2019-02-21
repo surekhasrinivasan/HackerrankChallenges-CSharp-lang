b49363cfcd44d45f9c0c9d63c3bad734bc2462a1
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-21 11:28:08 -0500

    Day9-Recursion - solved

--- 30DaysOfCode/Day9-Recursion/Day9-Recursion/Program.cs
+++ 30DaysOfCode/Day9-Recursion/Day9-Recursion/Program.cs
@@ -9,6 +9,35 @@ namespace Day9_Recursion
     // Write a factorial function that takes a positive integer, N as a parameter and prints the result of N! (N factorial).
     class Program
     {
+        //// Complete the factorial function below.
+        //static int factorial(int n)
+        //{
+        //    // Base Case
+        //    if (n <= 1)
+        //    {
+        //        return 1;
+        //    }
+        //    // Recursive Case
+        //    else
+        //    {
+        //        return n * factorial(n - 1);
+        //    }
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        //    int n = Convert.ToInt32(Console.ReadLine());
+
+        //    int result = factorial(n);
+
+        //    textWriter.WriteLine(result);
+
+        //    textWriter.Flush();
+        //    textWriter.Close();
+        //}
+
         // Complete the factorial function below.
         static int factorial(int n)
         {
@@ -24,18 +53,18 @@ namespace Day9_Recursion
             }
         }
 
+        // Main function
         static void Main(string[] args)
         {
-            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
+            Console.WriteLine("Enter an integer: ");
+        
             int n = Convert.ToInt32(Console.ReadLine());
 
             int result = factorial(n);
 
-            textWriter.WriteLine(result);
+            Console.WriteLine("The factorial of {0} is {1}", n, result);
 
-            textWriter.Flush();
-            textWriter.Close();
+            Console.WriteLine();
         }
     }
 }
