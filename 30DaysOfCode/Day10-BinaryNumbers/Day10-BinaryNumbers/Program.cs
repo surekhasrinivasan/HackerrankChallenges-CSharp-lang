e2ab3e12d6ef3597343a9cb1fe29ef87eddf20ba
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-22 06:47:18 -0500

    Day10-BinaryNumbers - initial commit

---
+++ 30DaysOfCode/Day10-BinaryNumbers/Day10-BinaryNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10_BinaryNumbers
+{
+    // Given a base-10 integer, n, convert it to binary (base-2). 
+    // Then find and print the base-10 integer denoting the maximum number of consecutive 1's in n's binary representation.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                 
+        }
+    }
+}
