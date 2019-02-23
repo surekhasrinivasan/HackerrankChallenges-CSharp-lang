228468fc0c52e4007b1ddb6e4c297dc54497660e
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-23 16:41:34 -0500

    Day11-2DArrays - initial commit

---
+++ 30DaysOfCode/Day11-2DArrays/Day11-2DArrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11_2DArrays
+{
+    /*
+     * Given a 6 x 6 2D Array, A:
+        1 1 1 0 0 0
+        0 1 0 0 0 0
+        1 1 1 0 0 0
+        0 0 2 4 4 0
+        0 0 0 2 0 0
+        0 0 1 2 4 0
+      
+    * Calculate the hourglass sum for every hourglass in A, then print the maximum hourglass sum.
+    * 
+    * We define an hourglass in A to be a subset of values with indices falling in this pattern in A's graphical representation:
+      
+        a b c
+          d
+        e f g
+     * */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
