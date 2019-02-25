8a1a7f1527f1c63b81396e2aa89d64af64cf3413
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-24 21:23:37 -0500

    Created Book.cs, declared the properties

---
+++ LibraryCatalogue/LibraryCatalogue/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCatalogue
+{
+    // Book class
+    class Book
+    {
+        // Properties, Fields, Global variables
+        String title;
+        int pageCount;
+        int ISBN;
+        bool isCheckedOut; // whether or not the book is checked out
+        int dayCheckedOut = -1; // book is not checked out yet so it is -1 to start with       
+    }
+}
