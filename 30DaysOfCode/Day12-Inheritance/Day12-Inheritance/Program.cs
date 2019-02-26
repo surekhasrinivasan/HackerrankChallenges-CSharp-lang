ea5308a0e3ffa0b497f537caee69f0a417793147
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-25 22:08:14 -0500

    Day12-Inheritance - initial commit

---
+++ 30DaysOfCode/Day12-Inheritance/Day12-Inheritance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12_Inheritance
+{
+    /*
+     * You are given two classes, Person and Student, where Person is the base class and Student is the derived class. Completed code for Person and a declaration for Student are provided for you in the editor. Observe that Student inherits all the properties of Person.
+
+     * Complete the Student class by writing the following:
+
+        A Student class constructor, which has 4 parameters:
+        A string, firstName.
+        A string, lastName.
+        An integer, id.
+        An integer array (or vector) of test scores, scores.
+
+        A char calculate() method that calculates a Student object's average and returns the grade character representative of their calculated average:
+     **/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
