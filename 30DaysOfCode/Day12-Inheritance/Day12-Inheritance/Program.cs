ce04d09533d2fbea2f7675a1b1cfc6c72656901c
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-26 13:29:29 -0500

    Added Person class, wrote code for Student class

--- 30DaysOfCode/Day12-Inheritance/Day12-Inheritance/Program.cs
+++ 30DaysOfCode/Day12-Inheritance/Day12-Inheritance/Program.cs
@@ -6,8 +6,29 @@ using System.Threading.Tasks;
 
 namespace Day12_Inheritance
 {
+    class Person
+    {
+        protected string firstName;
+        protected string lastName;
+        protected int id;
+
+        public Person() { }
+        public Person(string firstName, string lastName, int identification)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.id = identification;
+        }
+        public void printPerson()
+        {
+            Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id);
+        }
+    }
+
     /*
-     * You are given two classes, Person and Student, where Person is the base class and Student is the derived class. Completed code for Person and a declaration for Student are provided for you in the editor. Observe that Student inherits all the properties of Person.
+     * You are given two classes, Person and Student, where Person is the base class and Student is the derived class. 
+     * Completed code for Person and a declaration for Student are provided for you in the editor. 
+     * Observe that Student inherits all the properties of Person.
 
      * Complete the Student class by writing the following:
 
@@ -19,6 +40,39 @@ namespace Day12_Inheritance
 
         A char calculate() method that calculates a Student object's average and returns the grade character representative of their calculated average:
      **/
+
+    class Student : Person
+    {
+        private int[] testScores;
+        /*	
+        *   Class Constructor
+        *   
+        *   Parameters: 
+        *   firstName - A string denoting the Person's first name.
+        *   lastName - A string denoting the Person's last name.
+        *   id - An integer denoting the Person's ID number.
+        *   scores - An array of integers denoting the Person's test scores.
+        */
+        // Write your constructor here
+        public Student() { }
+        public Student(string firstName, string lastName, int id, int[] testScores)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.id = id;
+            this.testScores = testScores;
+        }
+
+        /*	
+        *   Method Name: Calculate
+        *   Return: A character denoting the grade.
+        */
+        // Write your method here
+
+
+    }
+
+    // Main method
     class Program
     {
         static void Main(string[] args)
