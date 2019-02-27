07a7368f338abf6fa2f50e0be4f99a56ebe8173f
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-27 13:57:32 -0500

    Day14-Scope - initial commit

---
+++ 30DaysOfCode/Day14-Scope/Day14-Scope/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_Scope
+{
+    /*Complete the Difference class by writing the following:
+     * 
+     * A class constructor that takes an array of integers as a parameter and saves it to the elements instance variable.
+     * 
+     * A computeDifference method that finds the maximum absolute difference between any 2 numbers in N and stores it 
+     * in the maximumDifference instance variable.
+     * 
+     */
+     
+    class Difference
+    {
+        private int[] elements;
+        public int maximumDifference;
+
+        // Add your code here
+
+    } // End of Difference Class
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
