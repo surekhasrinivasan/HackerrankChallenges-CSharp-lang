80980f7a3bc26f5d329572ec17d25e589f01a1fb
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-28 11:51:33 -0500

    Day16-Exceptions-StringToInteger - initial commit

---
+++ 30DaysOfCode/Day16-Exceptions-StringToInteger/Day16-Exceptions-StringToInteger/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16_Exceptions_StringToInteger
+{
+    // Task
+    
+    // Read a string, S, and print its integer value; if S cannot be converted to an integer, print Bad String.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
