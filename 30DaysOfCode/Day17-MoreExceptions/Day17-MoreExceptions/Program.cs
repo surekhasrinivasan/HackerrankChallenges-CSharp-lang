1b355af095bd93f5c25d728a841ec6d8d283052e
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-01 13:28:55 -0500

    Day17-MoreExceptions - initial commit

---
+++ 30DaysOfCode/Day17-MoreExceptions/Day17-MoreExceptions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day17_MoreExceptions
+{
+    /* Write a Calculator class with a single method: int power(int,int). 
+     * The power method takes two integers, n and p, as parameters and returns the integer result of n^p. 
+     * If either n or p is negative, then the method must throw an exception with the message: n and p should be non-negative. 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator myCalculator = new Calculator();
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                string[] num = Console.ReadLine().Split();
+                int n = int.Parse(num[0]);
+                int p = int.Parse(num[1]);
+                try
+                {
+                    int ans = myCalculator.power(n, p);
+                    Console.WriteLine(ans);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+
+                }
+            }
+        }
+    }
+}
