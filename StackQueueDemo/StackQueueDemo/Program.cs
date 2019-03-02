7a4a8345796804fd417f3af8216359296b08067c
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-02 17:28:42 -0500

    StackQueueDemo - initial commit

---
+++ StackQueueDemo/StackQueueDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueueDemo
+{
+    //Program to demo Queue and Stacks
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
