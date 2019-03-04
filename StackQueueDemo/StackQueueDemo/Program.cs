6e085c6a6fac88067850134db66fef01beba96c8
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-03 23:39:17 -0500

    Implementation of Queue

--- StackQueueDemo/StackQueueDemo/Program.cs
+++ StackQueueDemo/StackQueueDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,30 @@ namespace StackQueueDemo
     {
         static void Main(string[] args)
         {
+            Queue q = new Queue();
+
+            q.Enqueue('A');
+            q.Enqueue('M');
+            q.Enqueue('G');
+            q.Enqueue('W');
+
+            Console.WriteLine("Current queue: ");
+            foreach (char c in q) Console.Write(c +" ");
+
+            Console.WriteLine();
+            q.Enqueue('V');
+            q.Enqueue('H');
+            Console.WriteLine("Current queue: ");
+            foreach (char c in q) Console.Write(c + " ");
+
+            Console.WriteLine();
+            Console.WriteLine("Removing some values ");
+            char ch = (char)q.Dequeue();
+            Console.WriteLine("The removed value: {0}", ch);
+            ch = (char)q.Dequeue();
+            Console.WriteLine("The removed value: {0}", ch);
+
+            Console.Read();
         }
     }
 }
