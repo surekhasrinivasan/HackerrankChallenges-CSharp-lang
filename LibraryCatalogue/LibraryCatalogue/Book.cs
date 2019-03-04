57a3d22de7174b499707c47cf22db1dc48cf6a31
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-03 22:14:45 -0500

    Created Setter Methods

---
+++ LibraryCatalogue/LibraryCatalogue/Book.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCatalogue
+{
+    // Book class
+    class Book
+    {
+        // Properties, Fields, Global variables
+        String title;
+        int pageCount;
+        int ISBN;
+        bool isCheckedOut; // whether or not the book is checked out
+        int dayCheckedOut = -1; // book is not checked out yet so it is -1 to start with   
+
+        // Constructor 
+        public Book(string bookTitle, int bookPageCount, int bookISBN)
+        {
+            this.title = bookTitle;
+            this.pageCount = bookPageCount;
+            this.ISBN = bookISBN;
+            isCheckedOut = false;
+        }
+
+        // Getters -- instance methods
+        public string getTitle()
+        {
+            return this.title;
+        }
+
+        public int getPageCount()
+        {
+            return this.pageCount;
+        }
+
+        public int getISBN()
+        {
+            return this.ISBN;
+        }
+
+        public bool getIsCheckedOut()
+        {
+            return this.isCheckedOut;
+        }
+
+        public int getDayCheckedOut()
+        {
+            return this.dayCheckedOut;
+        }
+
+        // Setters 
+        public void setIsCheckedOut(bool newIsCheckedOut, int currentDayCheckedOut)
+        {
+            this.isCheckedOut = newIsCheckedOut;
+            setDayCheckedOut(currentDayCheckedOut);
+        }
+
+        private void setDayCheckedOut(int day)
+        {
+            this.dayCheckedOut = day;
+        }
+    }
+}
