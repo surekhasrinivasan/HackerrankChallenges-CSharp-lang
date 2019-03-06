ebbe473f71ea34989056ddfb3e6ac600d4f13ef6
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-05 20:55:38 -0500

    LibraryCatalogue - modified files

--- LibraryCatalogue/LibraryCatalogue/Properties/AssemblyInfo.cs
+++ LibraryCatalogue/LibraryCatalogue/Properties/AssemblyInfo.cs
@@ -21,7 +21,11 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 <<<<<<< HEAD
+<<<<<<< HEAD
 [assembly: Guid("d9592ad4-15ee-41b0-ac5f-9acd7a3f67b0")]
+=======
+[assembly: Guid("c61a639f-b63b-49d9-a621-e23a3e3f3474")]
+>>>>>>> 30DaysOfCode
 =======
 [assembly: Guid("c61a639f-b63b-49d9-a621-e23a3e3f3474")]
 >>>>>>> 30DaysOfCode
