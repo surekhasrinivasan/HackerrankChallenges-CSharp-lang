bd70487b81de7420c4fd61217b63515716aa208c
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-06 14:08:21 -0500

    Day20-Sorting - Solved

--- 30DaysOfCode/Day20-Sorting/Day20-Sorting/Program.cs
+++ 30DaysOfCode/Day20-Sorting/Day20-Sorting/Program.cs
@@ -25,6 +25,36 @@ namespace Day20_Sorting
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the number of elements in an array: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the space-separated elements of the array: ");
+            string[] a_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+
+            int numSwaps = 0;
+
+            // Write Your Code Here
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n - 1; j++)
+                {
+                    if(a[j] > a[j + 1])
+                    {
+                        int temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                        numSwaps++;
+                    }                    
+                }
+                if(numSwaps == 0)
+                {
+                    break;
+                }                    
+            }            
+            Console.WriteLine("Array is sorted in {0} swaps.", numSwaps);
+            Console.WriteLine("First Element: {0}", a[0]);
+            Console.WriteLine("Last Element: {0}", a[n - 1]);
         }
     }
 }
