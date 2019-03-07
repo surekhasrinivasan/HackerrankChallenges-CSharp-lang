ef300064265def08ef6adaf9785c098ed4b615bc
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-07 12:06:02 -0500

    DrawBoard - initial commit

---
+++ DrawBoard/DrawBoard/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawBoard
+{
+    /* Task
+     * 
+     * Create the following function:
+     * 
+     * drawBoard(int x, int y)
+     * This function should display an ascii grid that is is x Rows wide ad y columns tall.
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
