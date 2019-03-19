aff43e4974215dacb6fff9a1e182c2aad2e43279
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-18 22:41:50 -0400

    Method IsUnique - completed

--- UniqueStringChallenge/UniqueStringChallenge/Program.cs
+++ UniqueStringChallenge/UniqueStringChallenge/Program.cs
@@ -11,6 +11,20 @@ namespace UniqueStringChallenge
     {
         public static bool IsUnique(string str)
         {
+            string temp1 = "";
+            string temp2 = "";
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                temp1 = str.Substring(i, 1);
+
+                for (int k = 0; k < str.Length; k++)
+                {
+                    temp2 = str.Substring(k, 1);
+                    if ((temp1 == temp2) && (i != k))
+                        return false;
+                }
+            }
             return true;
         }
         static void Main(string[] args)
