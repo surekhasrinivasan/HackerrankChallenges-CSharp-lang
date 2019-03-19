f7035de27f1cf76e5d93c378b772e3270765d970
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-18 22:31:40 -0400

    UniqueStringChallenge - initial commit

---
+++ UniqueStringChallenge/UniqueStringChallenge/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueStringChallenge
+{
+    // Implement an algorithm to determine if a string has all unique characters.    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
