700384ac8df636841319a441328f7921abc1f54d
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-18 22:36:32 -0400

    Created a method IsUnique to check if the string has all unique characters

--- UniqueStringChallenge/UniqueStringChallenge/Program.cs
+++ UniqueStringChallenge/UniqueStringChallenge/Program.cs
@@ -9,8 +9,14 @@ namespace UniqueStringChallenge
     // Implement an algorithm to determine if a string has all unique characters.    
     class Program
     {
+        public static bool IsUnique(string str)
+        {
+            return true;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine(IsUnique("Samarth"));
+            Console.WriteLine(IsUnique("Smith"));
         }
     }
 }
