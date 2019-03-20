4f6eb685363024616ab73bceda3b7bf6c495740c
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-19 22:05:42 -0400

    FizzBuzzChallenge -initial commit

---
+++ FizzBuzzChallenge/FizzBuzzChallenge/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzChallenge
+{
+    // Program prints from 1 to 100 and for multiples of 3, it prints Fizz and 
+    // for multiples of five it prints Buzz instead of numbers. For numbers which 
+    // are multiples of both 3 and 5, it prints FizzBuzz.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
