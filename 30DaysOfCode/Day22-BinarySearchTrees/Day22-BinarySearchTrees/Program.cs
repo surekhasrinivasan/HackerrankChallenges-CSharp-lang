7bab9addda6d4d2ada058ba36f230a83b4df3b24
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-31 16:11:53 -0400

    Code from Hackerrank challenge added

--- 30DaysOfCode/Day22-BinarySearchTrees/Day22-BinarySearchTrees/Program.cs
+++ 30DaysOfCode/Day22-BinarySearchTrees/Day22-BinarySearchTrees/Program.cs
@@ -31,10 +31,59 @@ namespace Day22_BinarySearchTrees
      * Sample Output
      * 3
      */
+
+    class Node
+    {
+        public Node left, right;
+        public int data;
+        public Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
     class Program
     {
+        static int getHeight(Node root)
+        {
+            //Write your code here
+        }
+
+        static Node insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
+
         static void Main(string[] args)
         {
+            Node root = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                root = insert(root, data);
+            }
+            int height = getHeight(root);
+            Console.WriteLine(height);
         }
     }
 }
