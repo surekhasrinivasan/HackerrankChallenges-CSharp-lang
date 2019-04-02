b380bc99c8d00d85b34e9e51704636a448896b6f
surekhasrinivasan <surekhasrao@gmail.com>
2019-04-01 22:44:48 -0400

    Day23-BSTLevelOrderTraversal - added Hackerrank provided code

--- 30DaysOfCode/Day23-BSTLevelOrderTraversal/Day23-BSTLevelOrderTraversal/Program.cs
+++ 30DaysOfCode/Day23-BSTLevelOrderTraversal/Day23-BSTLevelOrderTraversal/Program.cs
@@ -29,10 +29,58 @@ namespace Day23_BSTLevelOrderTraversal
      * Sample Output
      * 3 2 5 1 4 7 
      */
+
+    class Node
+    {
+        public Node left, right;
+        public int data;
+        public Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static void levelOrder(Node root)
         {
+            //Write your code here
+        }
+
+        static Node insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
+        static void Main(String[] args)
+        {
+            Node root = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                root = insert(root, data);
+            }
+            levelOrder(root);
         }
     }
 }
+
