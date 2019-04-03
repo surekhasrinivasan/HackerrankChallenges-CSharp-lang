edd8cf66af2cb901d0b56584028a3f59e3b45f5e
surekhasrinivasan <surekhasrao@gmail.com>
2019-04-03 16:20:19 -0400

    Day24-MoreLinkedLists - Added code provided by Hackerranck

--- 30DaysOfCode/Day24-MoreLinkedLists/Day24-MoreLinkedLists/Program.cs
+++ 30DaysOfCode/Day24-MoreLinkedLists/Day24-MoreLinkedLists/Program.cs
@@ -41,10 +41,67 @@ namespace Day24_MoreLinkedLists
      * 
      * 1 2 3 4 
      */
+
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+
+    }
+
     class Program
     {
+        public static Node removeDuplicates(Node head)
+        {
+            //Write your code here
+        }
+
+        public static Node insert(Node head, int data)
+        {
+            Node p = new Node(data);
+
+
+            if (head == null)
+                head = p;
+            else if (head.next == null)
+                head.next = p;
+            else
+            {
+                Node start = head;
+                while (start.next != null)
+                    start = start.next;
+                start.next = p;
+
+            }
+            return head;
+        }
+
+        public static void display(Node head)
+        {
+            Node start = head;
+            while (start != null)
+            {
+                Console.Write(start.data + " ");
+                start = start.next;
+            }
+        }
+
         static void Main(string[] args)
         {
+            Node head = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                head = insert(head, data);
+            }
+            head = removeDuplicates(head);
+            display(head);
         }
     }
 }
