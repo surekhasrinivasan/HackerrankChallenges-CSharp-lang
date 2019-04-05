a5ac2e02b6822a1e92f094987fbf10f52e9a04ba
surekhasrinivasan <surekhasrao@gmail.com>
2019-04-05 17:08:25 -0400

    Day25-RunningTimeAndComplexity challenge - initial commit

---
+++ 30DaysOfCode/Day25-RunningTimeAndComplexity/Day25-RunningTimeAndComplexity/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day25_RunningTimeAndComplexity
+{
+    /* Task 
+     * 
+     * A prime is a natural number greater than 1 that has no positive divisors other than 1 and itself. 
+     * Given a number, n, determine and print whether it's Prime or Not prime.
+     * 
+     * Input Format
+     * The first line contains an integer, T, the number of test cases. 
+     * Each of the T subsequent lines contains an integer, n, to be tested for primality.
+     * 
+     * Sample Input
+     * 
+     * 3
+     * 12
+     * 5
+     * 7
+     * 
+     * Sample Output
+     * 
+     * Not prime
+     * Prime
+     * Prime
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
