4001e9cf37d48509b547f4bd6af69b288082ac7f
surekhasrinivasan <surekhasrao@gmail.com>
2019-04-08 21:29:43 -0400

    Main method takes user input

--- CalculateGCDAndLCM/CalculateGCDAndLCM/Program.cs
+++ CalculateGCDAndLCM/CalculateGCDAndLCM/Program.cs
@@ -11,6 +11,11 @@ namespace CalculateGCDAndLCM
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
