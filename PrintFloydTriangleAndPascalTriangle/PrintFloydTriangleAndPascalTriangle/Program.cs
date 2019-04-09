6e1b290f3768fb83ddddbf6f4fa2421c28e34aa6
surekhasrinivasan <surekhasrao@gmail.com>
2019-04-09 17:06:59 -0400

    Printing Floyd's Triangle

--- PrintFloydTriangleAndPascalTriangle/PrintFloydTriangleAndPascalTriangle/Program.cs
+++ PrintFloydTriangleAndPascalTriangle/PrintFloydTriangleAndPascalTriangle/Program.cs
@@ -11,6 +11,18 @@ namespace PrintFloydTriangleAndPascalTriangle
     {
         static void Main(string[] args)
         {
+            // Floyd's Triangle 
+            Console.WriteLine("Floyd's Triangle Program: \n");
+            int i, j, k = 1;
+            for(i = 1; i <= 10; i++)
+            {
+                for(j = 1; j < i + 1; j++)
+                {
+                    Console.Write(k++ + " ");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadLine();
         }
     }
 }
