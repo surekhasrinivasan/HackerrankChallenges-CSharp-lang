036419c6c8d2cf9d5309b76880a58fdee28578c8
surekhasrinivasan <surekhasrao@gmail.com>
2019-04-08 21:37:42 -0400

    GetGCD method implemented

--- CalculateGCDAndLCM/CalculateGCDAndLCM/Program.cs
+++ CalculateGCDAndLCM/CalculateGCDAndLCM/Program.cs
@@ -9,6 +9,21 @@ namespace CalculateGCDAndLCM
     // Program to calculate Greatest Common Divisor and Least Common Multiple of 2 given number 
     class Program
     {
+        static int GetGCD(int number1, int number2)
+        {
+            while(number1 != number2)
+            {
+                if(number1 > number2)
+                {
+                    number1 = number1 - number2;
+                }
+                if(number2 > number1)
+                {
+                    number2 = number2 - number1;
+                }
+            }
+            return number1;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Enter first number: ");
@@ -16,6 +31,9 @@ namespace CalculateGCDAndLCM
 
             Console.WriteLine("Enter second number: ");
             int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int gcd = GetGCD(num1, num2);
+            Console.WriteLine("The greatest common divisor is: {0}", gcd);
         }
     }
 }
