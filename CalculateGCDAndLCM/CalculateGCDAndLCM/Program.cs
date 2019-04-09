928d362256ec87549f185c29d550b82ba4c2878e
surekhasrinivasan <surekhasrao@gmail.com>
2019-04-08 21:20:56 -0400

    Algorithm to calculate GCD and LCM - initial commit

---
+++ CalculateGCDAndLCM/CalculateGCDAndLCM/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateGCDAndLCM
+{
+    // Program to calculate Greatest Common Divisor and Least Common Multiple of 2 given number 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
