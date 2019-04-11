1983dc9b60256593c5d52149f31b57f683e78103
surekhasrinivasan <surekhasrao@gmail.com>
2019-04-10 21:26:01 -0400

    StringCountVowelsAndConsonants challenge - initial commit

---
+++ StringCountVowelsAndConsonants/StringCountVowelsAndConsonants/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCountVowelsAndConsonants
+{
+    // Program to count the number of Vowels and Consonants in a string 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
