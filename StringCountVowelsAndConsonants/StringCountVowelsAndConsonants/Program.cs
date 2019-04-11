ec01229c82d1a0784bb94a66b86523fe45b2ac8c
surekhasrinivasan <surekhasrao@gmail.com>
2019-04-10 22:14:12 -0400

    StringCountVowelsAndConsonants challenge - solved

--- StringCountVowelsAndConsonants/StringCountVowelsAndConsonants/Program.cs
+++ StringCountVowelsAndConsonants/StringCountVowelsAndConsonants/Program.cs
@@ -15,10 +15,33 @@ namespace StringCountVowelsAndConsonants
             
             int i, vowels = 0, consonants = 0, specialChars = 0, lengthOfTheString;
 
-            Console.WriteLine("Enter the length of the string to count its vowels and consonants: ");
+            Console.WriteLine("Enter the length of the string to count its vowels and consonants: \n");
             lengthOfTheString = Convert.ToInt32(Console.ReadLine());
 
-            
+            for (i = 0; i < lengthOfTheString; i++)
+            {
+                sentence[i] = Convert.ToChar(Console.Read());
+            }
+            for (i = 0; sentence[i] != '\0'; i++)
+            {
+                if(sentence[i] == 'a' || sentence[i] == 'e' || sentence[i] == 'i' || sentence[i] == 'o' || sentence[i] == 'u' ||
+                    sentence[i] == 'A' || sentence[i] == 'E' || sentence[i] == 'I' || sentence[i] == 'O' || sentence[i] == 'U')
+                {
+                    vowels = vowels + 1;
+                }
+                else
+                {
+                    consonants = consonants + 1;
+                }
+                if (sentence[i] == '\0' || sentence[i] == ' ')
+                {
+                   specialChars = specialChars + 1;
+                }                
+            }
+            consonants = consonants - specialChars;
+            Console.WriteLine("The number of vowels in the string is : {0}", vowels);
+            Console.WriteLine("The number of consonants in the string is: {0}", consonants);
+            Console.ReadLine();
         }
     }
 }
