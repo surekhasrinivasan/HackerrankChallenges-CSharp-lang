584d6e29a374a38b7d9cad95abb0a0aaca6fea6c
surekhasrinivasan <surekhasrao@gmail.com>
2019-04-14 22:29:37 -0400

    New clas ReplaceSpace is created which has a method for Replacing spaces

---
+++ StringChallenge3/StringChallenge3/ReplaceSpace.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringChallenge3
+{
+    public static class ReplaceSpace
+    {
+        public static string Replace(string s)
+        {
+        
+        }
+    }
+}
