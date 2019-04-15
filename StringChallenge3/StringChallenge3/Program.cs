f031cf4fe75457b75ff8e79426224437bd28d83f
surekhasrinivasan <surekhasrao@gmail.com>
2019-04-14 22:21:31 -0400

    Program to replace all spaces in a string to '%20'

---
+++ StringChallenge3/StringChallenge3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringChallenge3
+{
+    /* Program to Replace all spaces in a string with '%20'
+     * 
+     * The algorithm is as follows:
+     * Count the number of spaces during first scan of string
+     * Parse the string again from the end and for each character 
+     * if space is encountered, store”%20″ 
+     * else store the character as it is in newly shifted location
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
