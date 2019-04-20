2aed12cf2f0e2d73fb1a50f8e16f88aa730d61fe
surekhasrinivasan <surekhasrao@gmail.com>
2019-04-19 23:12:01 -0400

    Implemented Move method

--- CodingPuzzles/CodingPuzzles/MoveZeros.cs
+++ CodingPuzzles/CodingPuzzles/MoveZeros.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace CodingPuzzles
 {
-    class MoveZeros
+    public static class MoveZeros
     {
+        public static void Move(params int[] x)
+        {
+            for(int i = 0; i < x.Length; i++)
+            {
+                if(x[i] == 0)
+                {
+                    MoveZeroToEnd(x, i);
+                }
+            }
+        }
+
+        private static void MoveZeroToEnd(int[] x, int index)
+        {
+
+        }
     }
 }
