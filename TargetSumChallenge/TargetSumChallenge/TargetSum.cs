84e63e3e949ca131c0541f15ca260449cc61fab8
surekhasrinivasan <surekhasrao@gmail.com>
2019-04-21 21:39:17 -0400

    Created and implemented TwoIntegersSumToTarget method

--- TargetSumChallenge/TargetSumChallenge/TargetSum.cs
+++ TargetSumChallenge/TargetSumChallenge/TargetSum.cs
@@ -8,6 +8,21 @@ namespace TargetSumChallenge
 {
     public static class TargetSum
     {
-
+        public static bool TwoIntegersSumToTarget(int[] arr, int target)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                for(int k = 0; k < arr.Length; k++)
+                {
+                    if(i != k)
+                    {
+                        int sum = arr[i] + arr[k];
+                        if (sum == target)
+                            return true;
+                    }
+                }
+            }
+            return false;
+        }
     }
 }
