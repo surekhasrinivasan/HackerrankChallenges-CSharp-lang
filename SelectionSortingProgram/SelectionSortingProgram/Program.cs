459709dc43fcfde31e584451b6cf40927695be40
surekhasrinivasan <surekhasrao@gmail.com>
2019-04-27 23:19:33 -0400

    Display of unsorted array

--- SelectionSortingProgram/SelectionSortingProgram/Program.cs
+++ SelectionSortingProgram/SelectionSortingProgram/Program.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace SelectionSortingProgram
 {
+    // Program to Perform a Selection Sort
     class Program
     {
         static void Main(string[] args)
         {
+            int array_size = 10;
+            int[] array = new int[10] { 100, 50, 20, 40, 10, 60, 80, 70, 90, 30 };
+            Console.WriteLine("The array before Selecton sort is :");
+            for (int i = 0; i < array_size; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
         }
     }
 }
