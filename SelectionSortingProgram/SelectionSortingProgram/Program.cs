9efda33098b3dc88716201cb2fa983dc9a486426
surekhasrinivasan <surekhasrao@gmail.com>
2019-04-27 23:21:21 -0400

    Code to perform Selection sort

--- SelectionSortingProgram/SelectionSortingProgram/Program.cs
+++ SelectionSortingProgram/SelectionSortingProgram/Program.cs
@@ -18,6 +18,24 @@ namespace SelectionSortingProgram
             {
                 Console.WriteLine(array[i]);
             }
+            int tmp, min_key;
+
+            for (int j = 0; j < array_size - 1; j++)
+            {
+                min_key = j;
+
+                for (int k = j + 1; k < array_size; k++)
+                {
+                    if (array[k] < array[min_key])
+                    {
+                        min_key = k;
+                    }
+                }
+
+                tmp = array[min_key];
+                array[min_key] = array[j];
+                array[j] = tmp;
+            }
         }
     }
 }
