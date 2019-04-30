29f7ca7b1afe005719766af4cad5c9f92a9645f6
surekhasrinivasan <surekhasrao@gmail.com>
2019-04-29 21:56:17 -0400

    Armstrong number challenge - initial commit

---
+++ ArmstrongNumbers/ArmstrongNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmstrongNumbers
+{
+    // Program to Check Whether the Entered Number is an Armstrong Number or Not
+
+    // An Armstrong number of three digits is an integer such that the sum of the cubes of its digits is equal to the number itself.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
