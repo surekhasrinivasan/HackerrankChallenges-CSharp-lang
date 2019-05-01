b1bc2b4e07dcb9e57703b23a08848d6a9e8c66a2
surekhasrinivasan <surekhasrao@gmail.com>
2019-04-30 21:32:24 -0400

    Created a method called SolveTowers

--- TowersOfHanoi/TowersOfHanoi/Program.cs
+++ TowersOfHanoi/TowersOfHanoi/Program.cs
@@ -24,6 +24,19 @@ namespace TowersOfHanoi
     {
         static void Main(string[] args)
         {
+            //Solving the Towers of Hanoi challenge using Recursion
+
+
         }
+
+        public static void solveTowers(int n, char startPeg, char endPeg, char tempPeg)
+        {
+            if(n > 0)
+            {
+                solveTowers(n - 1, startPeg, tempPeg, endPeg);
+                Console.WriteLine("Move disk from " + startPeg+ ' '+endPeg);
+                solveTowers(n - 1, tempPeg, endPeg, startPeg);
+            }
+        } 
     }
 }
