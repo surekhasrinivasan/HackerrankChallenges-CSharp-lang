c7ae2122028fe5623589138e16ff042ddbb011b8
surekhasrinivasan <surekhasrao@gmail.com>
2019-05-01 21:15:16 -0400

    RemoveSpecialChars challenge - initial commit

---
+++ RemoveSpecialChars/RemoveSpecialChars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveSpecialChars
+{
+    // Program to remove special characters from a given string
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
