5583b4ded24c034e830bd2b882aa2460fe589c2b
surekhasrinivasan <surekhasrao@gmail.com>
2019-05-01 21:29:19 -0400

    Created and implementation of RemoveSpecialCharacters method

--- RemoveSpecialChars/RemoveSpecialChars/Program.cs
+++ RemoveSpecialChars/RemoveSpecialChars/Program.cs
@@ -11,6 +11,21 @@ namespace RemoveSpecialChars
     {
         static void Main(string[] args)
         {
+
+        }
+
+        public static string RemoveSpecialCharacters(string str)
+        {
+            string[] chars = new string[] { ",", ".", "/", "!", "@", "#", "$", "%", "^", "&", "*", "'", "\"", ";", "_", "(", ")", ":", "|", "[", "]" };
+            
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (str.Contains(chars[i]))
+                {
+                    str = str.Replace(chars[i], "");
+                }
+            }
+            return str;
         }
     }
 }
