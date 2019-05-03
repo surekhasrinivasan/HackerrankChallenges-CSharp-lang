117dbcbbb33d0323462e810c4e27f41193c6f2b7
surekhasrinivasan <surekhasrao@gmail.com>
2019-05-02 20:30:26 -0400

    NullDateTimeProgram - initial commit

---
+++ NullDateTimeProgram/NullDateTimeProgram/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullDateTimeProgram
+{
+    // Program to assign null value to Datetime
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
