34d61738d2d6886ccee1b6946e2522f83da1e009
surekhasrinivasan <surekhasrao@gmail.com>
2019-05-06 21:56:20 -0400

    Demo Empty statment - initial commit

---
+++ EmptyStatement/EmptyStatement/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyStatement
+{
+    // Program to demo Empty statement.
+
+    // Empty statements are those that does not contain anything other than a semicolon(;)s   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
