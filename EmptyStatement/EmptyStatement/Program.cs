122273d6fad54189c720c864c88fa3f1381ce152
surekhasrinivasan <surekhasrao@gmail.com>
2019-05-08 21:48:41 -0400

    Demo of Example-2

--- EmptyStatement/EmptyStatement/Program.cs
+++ EmptyStatement/EmptyStatement/Program.cs
@@ -9,24 +9,34 @@ namespace EmptyStatement
     // Program to demo Empty statement.
 
     // Empty statements are those that does not contain anything other than a semicolon(;)s   
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        int i = 0;
+    //        Program p = new Program();
+    //        while (p.Print())
+    //        {
+    //            ; // Empty statement
+    //        }
+    //        Console.WriteLine("i = {0}", i);
+    //        Console.ReadLine();
+    //    }
+
+    //    public bool Print()
+    //    {
+    //        Console.WriteLine("CSharp");
+    //        return true;
+    //    }
+    //}
+
+    // Example - 2 Use an Empty statement as the body of the while loop
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            int i = 0;
-            Program p = new Program();
-            while (p.Print())
-            {
-                ; // Empty statement
-            }
-            Console.WriteLine("i = {0}", i);
-            Console.ReadLine();
-        }
 
-        public bool Print()
-        {
-            Console.WriteLine("CSharp");
-            return true;
         }
     }
+
 }
