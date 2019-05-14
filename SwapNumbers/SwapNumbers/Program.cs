b0de14dec48ec2ad32d5e1a69ebbb6a19e8427af
surekhasrinivasan <surekhasrao@gmail.com>
2019-05-13 21:31:07 -0400

    Program to swap two numbers - initial commit

---
+++ SwapNumbers/SwapNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapNumbers
+{
+    // Program to swap numbers without using a temp variable 
+
+    // Method I
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
