9d4e7edabd8db620d325b178050dac3df0d7ab9a
surekhasrinivasan <surekhasrao@gmail.com>
2019-05-13 21:38:14 -0400

    Method-II to swap numbers using XOR operator

--- SwapNumbers/SwapNumbers/Program.cs
+++ SwapNumbers/SwapNumbers/Program.cs
@@ -9,19 +9,30 @@ namespace SwapNumbers
     // Program to swap numbers without using a temp variable 
 
     // Method I
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        int first, second;
+    //        first = 100;
+    //        second = 200;
+    //        first = first + second;
+    //        second = first - second;
+    //        first = first - second;
+    //        Console.WriteLine(first.ToString());
+    //        Console.WriteLine(second.ToString());
+    //        Console.ReadLine();
+    //    }
+    //}
+
+    // Method II Program to swap numbers using XOR operator:
+
     class Program
     {
         static void Main(string[] args)
         {
-            int first, second;
-            first = 100;
-            second = 200;
-            first = first + second;
-            second = first - second;
-            first = first - second;
-            Console.WriteLine(first.ToString());
-            Console.WriteLine(second.ToString());
-            Console.ReadLine();
+            
         }
     }
+
 }
