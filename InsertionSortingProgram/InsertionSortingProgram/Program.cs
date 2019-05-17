2e32d90e6d11a8a3b06f87ac8b85bb45686ae25d
surekhasrinivasan <surekhasrao@gmail.com>
2019-05-16 21:26:30 -0400

    InsertionSortingProgram - initial commit

---
+++ InsertionSortingProgram/InsertionSortingProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSortingProgram
+{
+    /* The Insertion sort algorithm views the data in two halves.
+     * 
+     * The left half of sorted elements and the right half of elements to be sorted.
+     * 
+     * In each iteration, one element from the right half is taken and added to the left half so that the left half is still sorted.
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
