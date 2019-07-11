bc80533d5b5f7d3353f403a2ac048706abee0fad
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-10 23:26:30 -0400

    EnumExample - initial commit

---
+++ EnumExample/EnumExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumExample
+{
+    // Program to demo usage of Enum
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
