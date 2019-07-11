0bf3e8147f4a46db2e5e038727c5baf636d60732
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-10 23:37:20 -0400

    GetGender method declared and implemented

--- EnumExample/EnumExample/Program.cs
+++ EnumExample/EnumExample/Program.cs
@@ -13,17 +13,35 @@ namespace EnumExample
         {
         }
 
-        public enum Gender
+        public static string GetGender(Gender gender)
         {
-            Unknown = 0,
-            Male = 1,
-            Female = 2
-        }
-
-        public class Customer
-        {
-            public string Name { get; set; }
-            public Gender Gender { get; set; }
+            // The switch here is now more readable and maintainable because
+            // of replacing the integral numbers with Gender enum
+            switch (gender)
+            {
+                case Gender.Unknown:
+                    return "Unknown";
+                case Gender.Male:
+                    return "Male";
+                case Gender.Female:
+                    return "Female";
+                default:
+                    return "Invalid Data for Gender";
+            }
         }
     }
+
+    public enum Gender
+    {
+        Unknown = 0,
+        Male = 1,
+        Female = 2
+    }
+
+    public class Customer
+    {
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+    }
 }
+
