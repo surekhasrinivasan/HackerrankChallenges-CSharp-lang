9eb683cf15021914c1df5fd40b2529a268d40708
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-11 21:10:24 -0400

    EnumExample - completed

--- EnumExample/EnumExample/Program.cs
+++ EnumExample/EnumExample/Program.cs
@@ -11,6 +11,28 @@ namespace EnumExample
     {
         static void Main(string[] args)
         {
+            Customer[] customers = new Customer[3];
+            customers[0] = new Customer()
+            {
+                Name = "Mark",
+                Gender = Gender.Male
+            };
+
+            customers[1] = new Customer()
+            {
+                Name = "Mary",
+                Gender = Gender.Female
+            };
+
+            customers[2] = new Customer()
+            {
+                Name = "Sam",
+                Gender = Gender.Unknown
+            };
+            foreach(Customer customer in customers)
+            {
+                Console.WriteLine("Name = {0} && Gender = {1}", customer.Name, GetGender(customer.Gender));
+            }
         }
 
         public static string GetGender(Gender gender)
