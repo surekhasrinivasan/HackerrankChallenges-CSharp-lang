193a8902feaec92fe614dfd286856896d90e77f6
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-12 21:51:11 -0400

    ArmstrongNumberAlgorithm - initial commit

---
+++ ArmstrongNumberAlgorithm/ArmstrongNumberAlgorithm/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmstrongNumberAlgorithm
+{
+    // Program to check whether the number is Armstrong or not. 
+
+    // Armstrong number is a number that is equal to the sum of cubes of its digits. 
+    // For example 0, 1, 153, 370, 371 and 407 are the Armstrong numbers.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
