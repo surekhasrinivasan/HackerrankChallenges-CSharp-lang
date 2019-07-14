f2d3adaee52d2cf158d776dd3cf1d83b56fcf717
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-13 22:43:16 -0400

    SumDigitsAlgorithm - initial commit

---
+++ SumDigitsAlgorithm/SumDigitsAlgorithm/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumDigitsAlgorithm
+{
+    // Program to sum the digits in c#
+    // Example: 234 - Sum of the digits = 9
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
