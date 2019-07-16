fc61b593881d4fb607fda39113305c43058c2c30
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-15 20:49:04 -0400

    Implemented logic

--- PrintAlphabetTriangle/PrintAlphabetTriangle/Program.cs
+++ PrintAlphabetTriangle/PrintAlphabetTriangle/Program.cs
@@ -14,7 +14,15 @@ namespace PrintAlphabetTriangle
             char ch = 'A';
             int i, j, k, m;
 
-            
+            for(i = 1; i <=5; i++)
+            {
+                for(j=5;j>=i;j--)
+                    Console.Write(" ");
+                for(k=1;k<=i;k++)
+                    Console.Write(ch++);
+                ch--;
+
+            }
         }
     }
 }
