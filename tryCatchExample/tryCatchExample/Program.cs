afe6d02488515791d58d2258bb111f371b6be974
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-17 22:00:17 -0400

    try implemented

--- tryCatchExample/tryCatchExample/Program.cs
+++ tryCatchExample/tryCatchExample/Program.cs
@@ -12,10 +12,18 @@ namespace tryCatchExample
         static void Main(string[] args)
         {
             // Example without try/catch
-            int a = 10;
-            int b = 0;
-            int x = a / b;
-            Console.WriteLine("Rest of the code");
+            //int a = 10;
+            //int b = 0;
+            //int x = a / b;
+            //Console.WriteLine("Rest of the code");
+
+            // Example for try/catch
+            try
+            {
+                int a = 10;
+                int b = 0;
+                int x = a / b;
+            }
         }
     }
 }
