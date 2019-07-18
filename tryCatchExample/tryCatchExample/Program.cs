d22c5bfce335d459b5e6883f844c1a5cc25531b5
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-17 21:14:06 -0400

    tryCatchExample - initial commit

---
+++ tryCatchExample/tryCatchExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryCatchExample
+{
+    // Program to demo try catch exception
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
