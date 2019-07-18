d3f91c841ed26bb8c271cb4623eeea4177c717cf
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-17 21:50:19 -0400

    Example program without try/catch

--- tryCatchExample/tryCatchExample/Program.cs
+++ tryCatchExample/tryCatchExample/Program.cs
@@ -11,6 +11,11 @@ namespace tryCatchExample
     {
         static void Main(string[] args)
         {
+            // Example without try/catch
+            int a = 10;
+            int b = 0;
+            int x = a / b;
+            Console.WriteLine("Rest of the code");
         }
     }
 }
