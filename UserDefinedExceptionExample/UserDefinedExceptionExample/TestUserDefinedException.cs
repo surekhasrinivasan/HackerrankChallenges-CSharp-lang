b610a045e9f31ba566195a8324f26678b6bbe649
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-19 21:02:11 -0400

    declare InvalidAgeException class'

---
+++ UserDefinedExceptionExample/UserDefinedExceptionExample/TestUserDefinedException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserDefinedExceptionExample
+{
+    // C# allows us to create user-defined or custom 
+    // exception. It is used to make the meaningful 
+    // exception. To do this, we need to inherit Exception class.
+    public class InvalidAgeException : Exception
+    {
+        public InvalidAgeException(String message) : base(message)
+        {
+
+        }
+    }
+
+    class TestUserDefinedException
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
