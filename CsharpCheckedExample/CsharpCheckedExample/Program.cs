51c25b29271dc02226472f52c16be5138a70948a
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-20 21:30:17 -0400

    CsharpCheckedExample - initial commit

---
+++ CsharpCheckedExample/CsharpCheckedExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpCheckedExample
+{
+    // Program to demo example for Checked 
+
+    // The checked keyword is used to explicitly check overflow and conversion of integral type values at compile time.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
