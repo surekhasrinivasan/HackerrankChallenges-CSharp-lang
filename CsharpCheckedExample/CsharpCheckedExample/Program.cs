683f3ded894ddc37785d77c5b43c5ea8551e799d
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-20 22:21:33 -0400

    Csharp Unchecked Example using unchecked

--- CsharpCheckedExample/CsharpCheckedExample/Program.cs
+++ CsharpCheckedExample/CsharpCheckedExample/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CsharpCheckedExample
 {
-    // Program to demo example for Checked 
+    // Program to demo example for Checked and Unchecked
 
     // The checked keyword is used to explicitly check overflow and conversion of integral type values at compile time.
     class Program
@@ -18,10 +18,19 @@ namespace CsharpCheckedExample
             //Console.WriteLine(val + 2); // output : -2147483647
 
             // C# Checked Example using checked
-            checked
+            //checked
+            //{
+            //    int val = int.MaxValue;
+            //    Console.WriteLine(val + 2); // output: Unhandled Exception: System.OverflowException: Arithmetic operation resulted in an overflow.
+            //}
+
+            // The Unchecked keyword ignores the integral type arithmetic exceptions. It does not check 
+            // explicitly and produce result that may be truncated or wrong.
+
+            unchecked
             {
                 int val = int.MaxValue;
-                Console.WriteLine(val + 2); // output: Unhandled Exception: System.OverflowException: Arithmetic operation resulted in an overflow.
+                Console.WriteLine(val + 2); // output: -2147483647
             }
         }
     }
